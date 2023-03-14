7982be735466dd2d57215d897179933d774a5a35
Cendyne <cendyne@cendyne.dev>
2023-03-13 23:48:06 -0500

    Desktop file

--- root-install.sh
+++ root-install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 apt install libffi-dev libssl-dev git curl build-essential zlib1g-dev
+dir=$(pwd)
+cat << EOF /tmp/PrintServer.desktop
+#!/usr/bin/env xdg-open
 
+[Desktop Entry]
+Name=Print Server
+Encoding=UTF-8
+Exec=bash -c "cd $dir && $dir/start.sh"
+Type=Application
+Categories=Development;
+Terminal=true
+EOF
+
+desktop-file-install /tmp/PrintServer.desktop
