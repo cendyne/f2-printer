c799792b2546aec52b38a2a8a6508c5c43292c1e
Cendyne <cendyne@cendyne.dev>
2024-01-17 14:17:51 -0600

    Remove keyring call, rely on env instead

--- install.sh
+++ install.sh
@@ -43,9 +43,6 @@ export PATH="$HOME/.local/bin:$PATH"
 echo "Installing dependencies"
 poetry config virtualenvs.in-project true
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-if [ ! -f "$HOME/.config/python_keyring/keyringrc.cfg" ]; then
-  python -m keyring --disable
-fi
 poetry install || true
 
 echo ""
