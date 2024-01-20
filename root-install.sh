9565afd672f777d31d6ee6eb7b790295c5fca0b9
Cendyne <cendyne@cendyne.dev>
2024-01-20 16:48:44 -0600

    LP Admin, don't sleep, ignore hinge

--- root-install.sh
+++ root-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 apt install libffi-dev libssl-dev git curl build-essential zlib1g-dev tk-dev libsqlite3-dev libbz2-dev liblzma-dev libreadline-dev libncurses-dev libcupsimage2
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 dir=$(pwd)
-cat << EOF >> /tmp/PrintServer.desktop
+user=$(users)
+cat << EOF > /tmp/PrintServer.desktop
 #!/usr/bin/env xdg-open
 
 [Desktop Entry]
@@ -13,5 +15,13 @@ Type=Application
 Categories=Development;
 Terminal=true
 EOF
+
+cat << EOF > /etc/sudoers.d/printing
+%lpadmin ALL= NOPASSWD: /usr/bin/systemctl restart cups.service
+EOF
+
+sed -i 's/#HandleLidSwitch=ignore/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+
+usermod -a -G lpadmin "$user"
 
 desktop-file-install /tmp/PrintServer.desktop
